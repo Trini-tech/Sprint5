5b3b714bbd3e6bd7d55f422044a7fa54abb0cade
trini-tech <trinimarco02@gmail.com>
2023-10-11 20:26:22 +0200

    Exercici1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Card } from './Card.jsx'
+import { useState } from "react"
+
+
+export function App() {
+  const [step,setStep] = useState(0); 
+
+  const tutorialData = [
+  {
+    title: "Dedica moltes hores",
+    description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
+    bgColor: "#fff",
+    image: "",
+  },
+  {
+    title: "Programa projectes propis",
+    description: "Més val 10 hores treballant en projectes propis que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
+    bgColor: "#fff",
+    image: "",
+  },
+  {
+    title: "Procura descansar",
+    description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
+    bgColor: "#fff",
+    image: "",
+  }
+  ];
+
+  return (
+    <>
+    <Card {...tutorialData[step]}/>
+    </>
+  )
+}
---
+++ src/Card.jsx
@@ -0,0 +1,8 @@
+export function Card ({title, description, bgColor, image}){
+    return (
+        <>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        </>
+    )
+}
